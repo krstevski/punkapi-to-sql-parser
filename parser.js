2f070e133dbb5784dfcd30040d0e8146a2579c7e
Martin Krstevski <Martin.Krstevski@netcetera.com>
2022-04-20 14:07:34 +0200

    Cleaned up code

--- parser.js
+++ parser.js
@@ -16,12 +16,8 @@ const parseToSQL = (data) => {
   let insert_beer_query = [];
   for (beer of data) {
     const date_fields = beer.first_brewed.split("/");
-    let first_brewed;
-    if (date_fields.length > 1) {
-      first_brewed = `${date_fields[1]}-${date_fields[0]}-01`;
-    } else {
-      first_brewed = `${date_fields[0]}-01-01`;
-    }
+    const first_brewed = date_fields.length > 1 ? `${date_fields[1]}-${date_fields[0]}-01` : `${date_fields[0]}-01-01`;
+
     const abv = beer.abv ? `'${beer.abv}'` : null;
     const ibu = beer.ibu ? `'${beer.ibu}'` : null;
     const target_fg = beer.target_fg ? `'${beer.target_fg}'` : null;
@@ -34,16 +30,10 @@ const parseToSQL = (data) => {
     const beer_characteristics_query = `INSERT INTO beer_characteristics VALUES(default, ${abv}, ${ibu}, ${target_fg}, ${target_og}, ${ebc}, ${srm}, ${ph}, ${attenuation_level});`;
     const beer_volume_query = `INSERT INTO amount VALUES(default, '${beer.volume.value}', '${beer.volume.unit}');`;
     const beer_boil_volume_query = `INSERT INTO amount VALUES(default, '${beer.boil_volume.value}', '${beer.boil_volume.unit}');`;
-    const beer_query = `INSERT INTO beer VALUES(default, '${beer.name.replaceAll("'", "''")}', '${beer.tagline.replaceAll(
-      "'",
-      "''"
-    )}', '${first_brewed}', '${beer.description.replaceAll(
-      "'",
-      "''"
-    )}', ${image_url}, (SELECT max(id) FROM beer_characteristics), (SELECT max(id) - 1 FROM amount), (SELECT max(id) FROM amount), '${beer.brewers_tips.replaceAll(
-      "'",
-      "''"
-    )}', '${beer.contributed_by}');`;
+    const beer_query = `INSERT INTO beer VALUES(default, '${beer.name.replaceAll("'", "''")}', 
+    '${beer.tagline.replaceAll("'", "''")}', '${first_brewed}', '${beer.description.replaceAll("'", "''")}', ${image_url}, 
+      (SELECT max(id) FROM beer_characteristics), (SELECT max(id) - 1 FROM amount), (SELECT max(id) FROM amount),
+       '${beer.brewers_tips.replaceAll("'", "''")}', '${beer.contributed_by}');`;
     insert_beer_query.push(beer_characteristics_query);
     insert_beer_query.push(beer_volume_query);
     insert_beer_query.push(beer_boil_volume_query);
@@ -52,8 +42,8 @@ const parseToSQL = (data) => {
     const ingredients = beer.ingredients;
 
     for (const malt of ingredients.malt) {
-      const value = malt.amount.value ? `'${malt.amount.value}'` : null;
-      const malt_amount_query = `INSERT INTO amount VALUES(default, ${value}, '${malt.amount.unit}');`;
+      if (malt.amount.value === null) break;
+      const malt_amount_query = `INSERT INTO amount VALUES(default, '${malt.amount.value}', '${malt.amount.unit}');`;
       const malt_query = `INSERT INTO ingredient VALUES(default, '${malt.name}', 'malt', null, null, (SELECT max(id) FROM amount));`;
       const beer_ingredient_query = "INSERT INTO beer_ingredient VALUES((SELECT max(id) FROM beer), (SELECT max(id) FROM ingredient));";
       insert_beer_query.push(malt_amount_query);
@@ -62,8 +52,8 @@ const parseToSQL = (data) => {
     }
 
     for (const hops of ingredients.hops) {
-      const value = hops.amount.value ? `'${hops.amount.value}'` : null;
-      const hops_amount_query = `INSERT INTO amount VALUES(default, ${value}, '${hops.amount.unit}');`;
+      if (hops.amount.value === null) break;
+      const hops_amount_query = `INSERT INTO amount VALUES(default, '${hops.amount.value}', '${hops.amount.unit}');`;
       const hops_query = `INSERT INTO ingredient VALUES(default, '${hops.name}', 'hops', '${hops.add}', '${hops.attribute}', (SELECT max(id) FROM amount));`;
       const beer_ingredient_query = "INSERT INTO beer_ingredient VALUES((SELECT max(id) FROM beer), (SELECT max(id) FROM ingredient));";
       insert_beer_query.push(hops_amount_query);
@@ -79,26 +69,24 @@ const parseToSQL = (data) => {
     const method = beer.method;
 
     for (const mash_temp of method.mash_temp) {
+      if (mash_temp.temp.value === null) break;
       const duration = mash_temp.duration ? `'${mash_temp.duration}'` : null;
-      const value = mash_temp.temp.value ? `'${mash_temp.temp.value}'` : null;
-      const temp_amount_querty = `INSERT INTO amount VALUES(default, ${value}, '${mash_temp.temp.unit}');`;
+      const temp_amount_querty = `INSERT INTO amount VALUES(default, '${mash_temp.temp.value}', '${mash_temp.temp.unit}');`;
       const temp_query = `INSERT INTO method VALUES(default, 'mash_temp', (SELECT max(id) FROM amount), ${duration});`;
       const beer_method_query = "INSERT INTO beer_method VALUES((SELECT max(id) FROM beer), (SELECT max(id) FROM method));";
       insert_beer_query.push(temp_amount_querty);
       insert_beer_query.push(temp_query);
       insert_beer_query.push(beer_method_query);
     }
-    const fermentation_temp_value = method.fermentation.temp.value ? `'${method.fermentation.temp.value}'` : null;
-    const fermentation_temp_unit = method.fermentation.temp.unit ? `'${method.fermentation.temp.unit}'` : null;
 
-    const temp_amount_query = `INSERT INTO amount VALUES(default, ${fermentation_temp_value}, ${fermentation_temp_unit});`;
-    const fermentation_query = `INSERT INTO method VALUES(default, 'fermentation', (SELECT max(id) FROM amount), null);`;
-    const beer_method_query = "INSERT INTO beer_method VALUES((SELECT max(id) FROM beer), (SELECT max(id) FROM method));";
-    insert_beer_query.push(temp_amount_query);
-    insert_beer_query.push(fermentation_query);
-    insert_beer_query.push(beer_method_query);
-
-    // const twist_query = `INSERT INTO method VALUES(default, 'twist', null, )` // TODO: RESOLVE THIS
+    if (method.fermentation.temp.value !== null) {
+      const temp_amount_query = `INSERT INTO amount VALUES(default, '${method.fermentation.temp.value}', '${method.fermentation.temp.unit}');`;
+      const fermentation_query = `INSERT INTO method VALUES(default, 'fermentation', (SELECT max(id) FROM amount), null);`;
+      const beer_method_query = "INSERT INTO beer_method VALUES((SELECT max(id) FROM beer), (SELECT max(id) FROM method));";
+      insert_beer_query.push(temp_amount_query);
+      insert_beer_query.push(fermentation_query);
+      insert_beer_query.push(beer_method_query);
+    }
 
     for (const food of beer.food_pairing) {
       const food_cleaned = food.replaceAll("'", "''");
